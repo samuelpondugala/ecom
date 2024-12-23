e497f1142d48c9a6f8dea33db5801d64a34b25cb
David Samuel <davidsamuel7143@gmail.com>
2024-12-24 00:17:19 +0530

    Added some code in forms.py for creating new user accounts in the website

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django import forms
 
 def home(request):
@@ -34,4 +35,20 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in user
+            user = authenticate(request, username= username, password= password)
+            login(request, user)
+            messages.success(request, ("You Have Registered Succesfully! Welcome!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Whoops! There was a problem Registering, Please try again..."))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
